921c3ff99129434c0e07bd948fb10ec1e9e51266
ryan <ryannunez827@gmail.com>
2024-03-30 01:22:56 +0800

    inital commit

---
+++ src/components/CardFooter.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <hr class="border-1 my-8 border-gray-400 sm:w-4/12 mx-auto">
+        <div class="text-center">
+            <a href="https://www.facebook.com/ryz123" target="_blank"
+                class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"><font-awesome-icon
+                    icon="fa-brands fa-facebook" /></a>
+            <a href="https://www.instagram.com/ry.nunez/" target="_blank"
+                class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"><font-awesome-icon
+                    icon="fa-brands fa-instagram" /></a>
+            <a href="https://github.com/rynnzz" target="_blank"
+                class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"> <font-awesome-icon
+                    icon="fa-brands fa-github" /></a>
+            <a href="mailto:ryannunez827@gmail.com"
+                class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"> <font-awesome-icon
+                    icon="fa-solid fa-envelope" /></a>
+        </div>
+    </div>
+</template>
+<script setup></script>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="text-center">
+        <h2 class="text-center text-xl sm:text-2xl font-bold uppercase dark:text-white">Hi! Ryan Nunez </h2>
+        <p class="sm:text-center text-justify my-4 sm:w-4/12 mx-auto dark:text-gray-300">
+            Hello! I'm Ryan Nunez, a student in Cordova Public College, currently taking BSIT course, 21 years of age and a passionate developer with expertise in back end programming and a little bit of experience in front end programming. This portfolio showcases my projects, skills, and experiences in the field of Web Developing. Feel free to explore and get in touch!</p>
+        <p class="text-center text-blue-600">#BackEndProgramming #WebApplication</p>
+    </div>
+</template>
+<script setup>
+
+
+</script>
